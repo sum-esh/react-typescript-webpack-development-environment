aa1e3555b356e43d5ad5323062c03be5a4a12431
sumeshk789 <sumesh.k789@gmail.com>
2020-03-01 22:05:42 +0530

    package json and initila file

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+export class App extends React.Component<{}, {}>{
+    render() {
+        return <div className="h-100 w-100">
+            App
+            </div>
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import * as ReactDOM from 'react-dom'
+import 'tachyons'
+import { App } from './app'
+ReactDOM.render(<App />, document.getElementById('app'))
